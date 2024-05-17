6551afb81e09806eb0d2459d6bc2bc76a1745603
ohangl <angelachacongonzalez@gmail.com>
2024-05-16 21:49:42 -0300

    week3

---
+++ scripts/place.js
@@ -0,0 +1,20 @@
+
+
+let temp = 12;
+let windSpeed = 5;
+function windChill(temp, windSpeed) {
+    if (temp < 10 | temp == 10 & windSpeed > 4.8) {
+        return temp - (windSpeed * 0.7) + "°C";
+    } else {
+        return "N/A";
+}
+}
+document.getElementById("windChill").innerHTML = windChill(temp, windSpeed);
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('currentYear').textContent = new Date().getFullYear();
+    document.getElementById('lastModified').textContent = document.lastModified;
+
+
+});
